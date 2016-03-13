2e9db284877372d67f78c67d8eda8f71317483db
sakoh <sakohartounian@yahoo.com>
2016-03-13 15:01:31 -0700

    yml now works

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 var gulp = require('gulp'),
     jade = require('gulp-jade'),
     sass = require('gulp-sass'),
-    webserver = require('gulp-webserver')
+    webserver = require('gulp-webserver'),
+    yaml = require('js-yaml'),
+    fs = require('fs')
 
 gulp.task('jade', function() {
+    var content = yaml.safeLoad(fs.readFileSync('./content.yml', 'utf8'));
+
     gulp.src('./templates/index.jade')
-        .pipe(jade())
+        .pipe(jade({
+          locals: content
+        }))
         .pipe(gulp.dest('build'))
 
     gulp.src('./assets/**/*/**/*')
