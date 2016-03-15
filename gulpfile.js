0e17887e44f86befe8c7c60e9a95e2d56fba927a
sakoh <sakohartounian@yahoo.com>
2016-03-15 13:44:43 -0700

    indentation made more uniform in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,29 +6,35 @@ var gulp = require('gulp'),
     fs = require('fs')
 
 gulp.task('jade', () => {
-    var content = yaml.safeLoad(fs.readFileSync('./content.yml', 'utf8'));
+
+    var content = yaml.safeLoad(fs.readFileSync('./content.yml', 'utf8'))
 
     gulp.src('./templates/index.jade')
-        .pipe(jade({
-          locals: content
-        }))
-        .pipe(gulp.dest('build'))
+        .pipe(
+            jade({
+                locals: content
+            })
+        )
+        .pipe(gulp.dest('build')
 
     gulp.src('./assets/**/*/**/*')
         .pipe(gulp.dest('build/assets'))
+
 })
 
 gulp.task('webserver', () => {
-  gulp.src('./build')
-    .pipe(webserver({
-      livereload: true,
-      directoryListing: false,
-      open: true
-    }))
+
+    gulp.src('./build')
+        .pipe(
+            webserver({
+                livereload: true,
+                directoryListing: false,
+                open: true
+            })
+        )
+
 })
 
-
 gulp.watch(['./templates/**/*.jade','./content.yml'], ['jade'])
-// gulp.watch('./sass/**/*.scss', ['sass'])
 
 gulp.task('default', ['jade', 'webserver'])
